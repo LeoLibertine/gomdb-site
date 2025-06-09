51a9bfa1803ac2117fbc0bf6561c30e0a22d068c
Leon Libertine <ieo_20@hotmail.com>
2025-06-09 11:38:01 -0600

    Build y configuración de rewrites para Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/clientes/cencosud/document-mongo/',
-  plugins: [react()],
+  base: '/clientes/cencosud/document-mongo',
+  plugins: [react()]
 })
